0fdf6e13ffd567c313d3ba0e0f87ff4be123edc4
joshbabier <joshbabier@gmail.com>
2022-09-22 17:25:16 +0200

    chore: Generated component, class and util files for avatar

---
+++ app/utils/avatar.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-boring-avatars/utils/avatar';
---
+++ tests/unit/utils/avatar-test.js
@@ -0,0 +1,11 @@
+import avatar from 'dummy/utils/avatar';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | avatar', function() {
+
+  // TODO: Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = avatar();
+    assert.ok(result);
+  });
+});
